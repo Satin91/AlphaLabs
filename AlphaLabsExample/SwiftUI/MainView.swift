286ab7ce20ed36d20706417e3925676a02e47bb8
Satin91 <cool19912015@gmail.com>
2024-09-07 17:32:52 +0300

    Initial Commit

---
+++ AlphaLabsExample/SwiftUI/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  AlphaLabsExample
+//
+//  Created by Артур Кулик on 07.09.2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MainView()
+}
---
+++ AlphaLabsExample/UIKit/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AlphaLabsExample
+//
+//  Created by Артур Кулик on 07.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    func configureUI() {
+        self.view.backgroundColor = .blue
+    }
+}
+
